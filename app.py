bcbbe878abeebc47ea34c22f1887e9fd5d4c7601
mnbn <martin.blomgren@starstable.com>
2019-05-23 09:15:22 +0200

    Change logging format to json

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from aws_xray_sdk.core import patch_all
 
 import logging
+import json_logging
 import sys
 import os
 
@@ -15,17 +16,20 @@ service_environment = os.getenv('SERVICE_ENVIRONMENT', '-')
 prefix = ('/%s' % service_name)
 
 #Logging
+json_logging.ENABLE_JSON_LOGGING = True
+json_logging.init(framework_name='flask')
 stdout_handler = logging.StreamHandler(sys.stdout)
 handlers = [stdout_handler]
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(lineno)d:%(name)-8s %(message)s',
     handlers=handlers
 )
-LOGGER = logging.getLogger(__name__)
+json_logging.config_root_logger()
 
 app = Flask(__name__)
 api = Api(app)
+json_logging.init_request_instrument(app)
+LOGGER = logging.getLogger(__name__)
 LOGGER.info('start application - %s', service_name)
 
 xray_recorder.configure(service=service_name)
@@ -112,6 +116,7 @@ class User(Resource):
 
 @app.route(prefix + '/')
 def main_index():
+    LOGGER.info('main_index', extra = {'props' : {'name' : service_name, 'version' : service_version, 'environment' : service_environment}})
     return 'NAME: %s\nVERSION: %s\nENVIRONMENT: %s\n' % (service_name, service_version, service_environment)
 
 api.add_resource(User, prefix + '/user/<string:name>')
