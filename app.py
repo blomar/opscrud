117c25dc748e6712dda48c87ef3f4be26a8face8
blomar <martin.blomgren@starstable.com>
2020-03-17 15:11:33 +0100

    Metrics RequestLatency sent to cloudwatch

--- app.py
+++ app.py
@@ -1,41 +1,41 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request, session
 from flask_restful import Api, Resource, reqparse
 from healthcheck import HealthCheck, EnvironmentDump
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from aws_xray_sdk.core import patch_all
 
+from fluentmetrics import FluentMetric
+
 from time import sleep
 import random
 import logging
-import json_logging
 import sys
 import os
 
 service_name = os.getenv('SERVICE_NAME', '-')
 service_version = os.getenv('SERVICE_VERSION', '-')
 service_environment = os.getenv('SERVICE_ENVIRONMENT', '-')
+service_type = os.getenv('SERVICE_TYPE', '-')
 app_mesh_version = os.getenv('APPMESH_VERSION', '-')
 prefix = ('/%s' % service_name)
 
 db_password = os.getenv('DB_PASSWORD', '-')
 
-#Logging
-json_logging.ENABLE_JSON_LOGGING = True
-json_logging.init(framework_name='flask')
+# Logging
 stdout_handler = logging.StreamHandler(sys.stdout)
 handlers = [stdout_handler]
 logging.basicConfig(
     level=logging.INFO,
     handlers=handlers
 )
-json_logging.config_root_logger()
 
 app = Flask(__name__)
 api = Api(app)
-json_logging.init_request_instrument(app)
 LOGGER = logging.getLogger(__name__)
 LOGGER.info('start application - %s', service_name)
+
+m = FluentMetric().with_namespace(f'SSO/{service_type}/{service_name}').with_stream_id('Performance')
 
 xray_recorder.configure(service=service_name)
 XRayMiddleware(app, xray_recorder)
@@ -119,18 +119,33 @@ class User(Resource):
         users = [user for user in users if user['name'] != name]
         return '{} is deleted.'.format(name), 200
 
+
+@app.before_request
+def before_request_func():
+    m.with_timer('RequestLatency').with_dimension('path', request.path)
+
+@app.after_request
+def after_request_func(response):
+    m.elapsed(MetricName='RequestLatency', TimerName='RequestLatency')
+    return response
+
 @app.route(prefix + '/')
 def main_index():
-    LOGGER.info('main_index', extra = {'props' : {'name' : service_name, 'version' : service_version, 'environment' : service_environment, 'db_password': db_password}})
-    return '\nNAME: %s\nVERSION: %s\nENVIRONMENT: %s\nAPPMESH_VERSION: %s \nDB_PASSWORD: %s' % (service_name, service_version, service_environment, app_mesh_version, db_password)
+    LOGGER.info('main_index', extra={
+        'props': {'name': service_name, 'version': service_version, 'environment': service_environment,
+                  'db_password': db_password}})
+    return '\nNAME: %s\nVERSION: %s\nENVIRONMENT: %s\nAPPMESH_VERSION: %s \nDB_PASSWORD: %s' % (
+    service_name, service_version, service_environment, app_mesh_version, db_password)
+
 
 @app.route(prefix + '/stats')
 def stats():
     random_float_number = random.uniform(0.01, 4.0)
-    if (random_float_number > 3.0):
+    if (random_float_number > 4.0):
         return ('ERROR %s' % random_float_number), 500
     else:
         sleep(random_float_number)
         return 'OK', 200
+
 
 api.add_resource(User, prefix + '/user/<string:name>')
